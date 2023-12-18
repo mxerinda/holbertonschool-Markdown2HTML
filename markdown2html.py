76ec1f802d82d352af7349dcf5903d94e4e19888
Mercilove Xerinda <124763281+mxerinda@users.noreply.github.com>
2023-12-18 11:38:21 +0200

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -1,90 +1,30 @@
+#!/usr/bin/env python3
 import sys
+import os
+import markdown
 
-
-def ol_parse(index, lines_read_list):
-    list_ol = ['<ol>\n']
-    while (index < len(lines_read_list)):
-        if lines_read_list[index][0] != '*':
-            break
-
-        data = lines_read_list[index].strip()
-        string_to_parsing = data.lstrip("*").strip()
-        list_ol.append(f'  <li>{string_to_parsing}</li>\n')
-        index += 1
-    list_ol.append('</ol>\n')
-    return (index, list_ol)
-
-
-def ul_parse(index, lines_read_list):
-    list_ul = ['<ul>\n']
-    while (index < len(lines_read_list)):
-        if lines_read_list[index][0] != '-':
-            break
-
-        data = lines_read_list[index].strip()
-        string_to_parsing = data.lstrip("-").strip()
-        list_ul.append(f'  <li>{string_to_parsing}</li>\n')
-        index += 1
-    list_ul.append('</ul>\n')
-    return (index, list_ul)
-
-
-def heading_parse(index, lines_read_list):
-    """heading tags"""
-    count_heading = 0
-    list_heading = []
-    min_level = 1
-    max_level = 6
-
-    while (index < len(lines_read_list)):
-        if lines_read_list[index][0] != '#':
-            break
-        
-        data = lines_read_list[index].strip()
-        heading_level = len(data) - len(data.lstrip('#'))
-
-        if min_level <= heading_level <= max_level:
-            string_to_parsing = data.lstrip("#").strip()
-            list_heading.append(f'<h{heading_level}>{string_to_parsing}</h{heading_level}>\n')
-
-        index += 1
-
-    return (index, list_heading)
-
-
-funtion_parsing = {
-    '#': heading_parse,
-    '-': ul_parse,
-    '*': ol_parse,
-}
-
-if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        sys.stderr.write('Usage: ./markdown2html.py README.md README.html\n')
-        sys.exit(1)
+def convert_markdown_to_html(markdown_file, html_file):
     try:
-        htmlTagList = []
-        with open(sys.argv[1], 'r') as markdownFile:
-            lines_read_list = markdownFile.readlines()
-            index = 0
-            while (index < len(lines_read_list)):
-                line = lines_read_list[index].strip()
-                first_char = line[0]
-                if first_char in funtion_parsing.keys():
-                    (index, htmlTag) = funtion_parsing[first_char](index, lines_read_list)
-                else:
-                    htmlTag = 'parrafo\n'
-                    index += 1
-                htmlTagList.append(htmlTag)
-
-            print(htmlTagList)
-
-        with open(sys.argv[2], 'w', encoding="utf-8") as html:
-            for htmlLines in htmlTagList:
-                for html_tag in htmlLines:
-                    if html_tag:
-                        html.write(html_tag)
-        sys.exit(0)
+        with open(markdown_file, 'r') as md_file:
+            md_content = md_file.read()
+            html_content = markdown.markdown(md_content)
+            with open(html_file, 'w') as html_out:
+                html_out.write(html_content)
     except FileNotFoundError:
-        sys.stderr.write('Missing {}\n'.format(sys.argv[1]))
+        print(f"Missing {markdown_file}", file=sys.stderr)
         sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py <MarkdownFile> <OutputFile>", file=sys.stderr)
+        sys.exit(1)
+
+    markdown_file = sys.argv[1]
+    html_file = sys.argv[2]
+
+    if not os.path.exists(markdown_file):
+        print(f"Missing {markdown_file}", file=sys.stderr)
+        sys.exit(1)
+
+    convert_markdown_to_html(markdown_file, html_file)
+    sys.exit(0)
